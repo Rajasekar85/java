24938daac107a34e8bab741733885df5ccdaa49d
rajeemech85 <rajeemech85@gmail.com>
2022-05-19 22:36:10 +0530

    ffgg

---
+++ src/org/fam/RajaDetails.java
@@ -0,0 +1,27 @@
+package org.fam;
+
+public class RajaDetails {
+	private void sonName() 
+	{
+		System.out.println("dhurv mithran");
+		}
+
+		private void fatherName() {
+		System.out.println("rajasekar");
+		}
+		private void motherName() {
+		System.out.println("suganya");
+
+		}
+		private void chithapaName() {
+		System.out.println("Pragadesh");
+
+		}
+		public static void main(String[] args) {
+			RajaDetails san = new RajaDetails();
+			san.sonName();
+			san.fatherName();
+			san.motherName();
+			san.chithapaName();
+		}
+}
